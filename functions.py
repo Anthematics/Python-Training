2f23ed35404fb250a5fb23f7a2f904cae1c2d4b0
Jason Harder <Jasonharder2@gmail.com>
2019-05-19 14:37:56 -0400

    Update functions.py

--- functions.py
+++ functions.py
@@ -16,3 +16,12 @@ def square_root(num):
 print(square_root(16))
 print(square_root(100))
 
+# Write your tip function here:
+def tip (total,percentage):
+	return percentage / 100 * total
+  
+# Uncomment these function calls to test your tip function:
+print(tip(10, 25))
+# should print 2.5
+print(tip(0, 100))
+# should print 0.0
