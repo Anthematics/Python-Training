fae42bd1a453052bb43edb264536a5bed440c23b
Jason Harder <Jasonharder2@gmail.com>
2019-05-25 15:41:46 -0400

    Update Lists.py

--- Lists.py
+++ Lists.py
@@ -12,4 +12,18 @@ print(list(list1))
 list2 = range(0,40,5)
 print(list(list2)) #returns [0, 5, 10, 15, 20, 25, 30, 35]
 
+first_names = ['Ainsley','Ben' , 'Chani', 'Depak']
+
+age = []
+
+age.append(42)
+
+all_ages = age + [32, 41, 29]
+
+name_and_age = zip(first_names,all_ages)
+
+ids = range(4)
+
+print(list(name_and_age))
+
 
