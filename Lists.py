0a0129ef9f2e3e24066fb6a880ca6cc5cd56f878
Jason Harder <Jasonharder2@gmail.com>
2019-05-25 15:26:09 -0400

    Update Lists.py

--- Lists.py
+++ Lists.py
@@ -6,3 +6,10 @@ prices = [5, 3, 4, 5, 4] + [4]
 
 print(prices)
 print(new_orders)
+
+list1 = range(5, 15, 3) #returns [5, 8, 11, 14]
+print(list(list1))
+list2 = range(0,40,5)
+print(list(list2)) #returns [0, 5, 10, 15, 20, 25, 30, 35]
+
+
