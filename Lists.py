9e9f1f066c171214eaa04d4bc05c94d546694a2f
Jason Harder <Jasonharder2@gmail.com>
2019-05-25 16:22:51 -0400

    Update Lists.py

--- Lists.py
+++ Lists.py
@@ -26,4 +26,18 @@ ids = range(4)
 
 print(list(name_and_age))
 
+last_semester_gradebook = [("politics", 80), ("latin", 96), ("dance", 97), ("architecture", 65)]
 
+subjects = ["physics", "calculus", 'poetry', 'history']
+
+grades = [98,97,85,88]
+
+subjects.append("computer science")
+
+grades.append(100)
+
+gradebook = list(zip(subjects,grades))
+
+gradebook.append(("visual arts", 93))
+
+print(list(gradebook))
