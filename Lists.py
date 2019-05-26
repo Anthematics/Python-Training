d7e833193115a5e65c88c8591a0d5acbb82579ab
Jason Harder <Jasonharder2@gmail.com>
2019-05-25 22:20:26 -0400

    Update Lists.py

--- Lists.py
+++ Lists.py
@@ -133,3 +133,29 @@ twin_beds = inventory.count('twin bed')
 print(inventory)
 inventory.sort()
 print(inventory)
+
+#2nd review exercise
+toppings = ['pepperoni','pineapple','cheese','sausage','olives','anchovies','mushrooms']
+
+prices = [2,6,1,3,2,7,2]
+
+num_pizzas = len(toppings)
+print('We sell ' + str(num_pizzas) + " different kinds of pizza")
+
+pizzas = list(zip(prices,toppings))
+print(pizzas)
+
+pizzas.sort()
+print(pizzas)
+
+cheapest_pizza= pizzas[0]
+print(cheapest_pizza)
+
+priciest_pizza=pizzas[-1]
+print(priciest_pizza)
+
+three_cheapest = pizzas[0:3]
+print(three_cheapest)
+
+num_two_dollar_slices = prices.count(2)
+print(num_two_dollar_slices)
